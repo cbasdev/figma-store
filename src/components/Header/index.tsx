1b3182c0109e80203debdadcd188bbb7403079cc
Sebastian Velez <info.cbasdev@gmail.com>
2023-01-29 01:55:00 -0500

    :lipstick: Responsive design home first changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,15 +3,15 @@ import Button from '../Button'
 const Header = () => {
   return (
     <header className='bg-figma-yellow absolute w-full z-10'>
-      <div className='flex justify-between py-5 px-20'>
-        <div className=' flex gap-2 mr-20'>
+      <div className='justify-between py-5 px-6 sm:px-20 flex'>
+        <div className='hidden sm:flex gap-2 mr-8 md:mr-20'>
           <Button text='Shop' />
           <Button text='About' />
         </div>
         <div>
           <h1 className='text-2xl'>THE FIGMA STORE</h1>
         </div>
-        <div className='flex gap-2 items-center'>
+        <div className='gap-2 items-center hidden lg:flex'>
           <Button text='UNITED STATES' />
           <Button text='CART 0' />
         </div>
